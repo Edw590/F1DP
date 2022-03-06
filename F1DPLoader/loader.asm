732365d87f607c9897eccd4a3255c861edbe2d3b
DADi590 <40151365+DADi590@users.noreply.github.com>
2022-03-06 16:55:22 +0000

    Add: Loader freeing code block and more
    Now the Loader aside from loading, also has code to unload the allocated
    code block. The address is stored in the end of the Code section of the
    game EXE. When main_() ends, there's a call to exit(). I'm redirecting
    that call to the part of the Loader that unloads the code block.
    
    Aside from that, the Loader now checks the BIN file length when looking
    for realMain(), so that if that function is not found, it doesn't crash
    and so it reports an error and is able to keep loading the game without
    any patches (since the Patcher didn't get to start).

--- F1DPLoader/loader.asm
+++ F1DPLoader/loader.asm
@@ -18,9 +18,12 @@
 ; under the License.
 
 
-; Loader to load (wow) the external code to memory inside the code process and execute it
+; Loader to load (wow) and unload the external code to memory inside the game process and execute it.
 
+;;;;;;;;;;;;;;;;;;;
+;;;;; Load
 
+; This below doesn't matter at all. It's just for MASM to work.
 .686p
 .model flat
 .stack 100h
@@ -45,22 +48,59 @@
 ; I did NOT check the Fallout 1 DOS EXE v1.1 official; MD5 hash: 6A41C641B789B44FD7BE1805CE030C9B, or any other that may exist.
 
 ; Note: I could call all functions with the relative offset, but I prefer to have it hard-coded here so less bugs happen.
-; This will only run in the beginning anyway, so no big deal with performance.
+; This will only run in the beginning anyway, so no big deal with a loss of performance.
 
-; All (or most of) these functions and file names belong to POSIX 1003.1 (filelength doesn't), the same used by Watcom,
-; the compiler used for the game.
-; When there's no register mention on the parameters, it means it's on the stack (don't forget the calling convention
-; is backwards on that).
+; All (or most of) these functions names belong to POSIX 1003.1 (filelength doesn't, for example), the same used by Watcom,
+; the compiler used to compile the game.
+; When there's no register mention on the parameters, it means it's on the stack (don't forget Watcom's calling convention
+; is backwards on that but is not on the registers).
 ; The return value is either a register name, or [stack] or (void). To know what's returned, go to the official docs.
 
-; ATTENTION: I'm NOT freeing the Patcher's allocated memory ANYwhere. It ALWAYS stays in memory. Though, as it's just 10 kB,
-; I'm not worrying much about it. DOSBox can have up to 63 MB, and at most 31 MB as recommended. And 10 kB is only 0.03% of
-; 31 MB, so doesn't seem to be a wow leak.
-; The reason I'm not freeing the memory is because I don't know where I should do it. I don't know if the program exits only
-; at the end of main() or if it has other exit points. Also, this way I don't need to store anything on the code segment or
-; anywhere else more obscure (as an idea from the one with the loader idea, xtll, DOS video memory).
+;;;;;;;;;;;;;;;;;;;
+;;;;; Unload
 
-pusha ; Save all registers
+; (This part is here just because there's no need to change it, so if I do change anything in the Load part, I don't
+; need to be correcting the SUB ESI instruction).
+
+; To be possible to put here a jump or a CALL
+nop
+nop
+nop
+nop
+nop
+
+; Save only EAX, since exit() does not return and only requires EAX as parameter.
+push    eax
+
+call    rightAfter2
+rightAfter2:
+pop     esi
+; The value below must be the address of the rightAfter1 label (or the POP ESI instruction, which is the same).
+; ATTENTION: that means if you change the Loader code or move it on the game EXE to somewhere else, it must be updated!!!
+sub     esi, 0EA2E5h ; ESI = Code section address
+
+mov     eax, [esi+0EAFFBh] ; The location of the code block address in the Code section
+
+lea     edi, [esi+0CADE9h] ; _nfree_(EAX = ptr) (void)
+call    edi
+
+; Get EAX back from main(), to execute exit() with that value
+push    eax
+
+; Do what was removed from main() (a call to exit()) - this function does not return
+lea     edi, [esi+0CA40Ah] ; exit_(EAX = status) (void)
+call    edi
+
+; Just for IDA to see this Unload part automatically as a block of code (note again that the program will never get
+; here because of exit()).
+ret
+
+; End of program
+
+;;;;;;;;;;;;;;;;;;;
+;;;;; Load
+
+; Save all registers
 push    eax
 push    ebx
 ;push    ecx - See the end to know why it's commented
@@ -69,10 +109,12 @@ push    esi
 push    edi
 push    ebp
 
-call    rightAfter
-rightAfter:
+call    rightAfter1
+rightAfter1:
 pop     esi
-sub     esi, 0EA2E0h ; ESI = Code section address --> ATTENTION: if you put the loader ANYwhere else in the game EXE, change this value!!!
+; The value below must be the address of the rightAfter1 label (or the POP ESI instruction, which is the same).
+; ATTENTION: that means if you change the Loader code or move it on the game EXE to somewhere else, it must be updated!!!
+sub     esi, 0EA30Fh ; ESI = Code section address
 
 lea     eax, [esi+12345678h] ; INIT_MSG
 push    eax
@@ -129,25 +171,37 @@ cmp     dword ptr [edx], 0053454Ch ; [!!!] VERSION HERE
 jne     errorWrongVer
 
 mov     [edx+4], esi ; Code section address stored in the block in the 2nd 4 bytes
-mov     eax, [esi+728F6h+1] ; Get the address of a mov with the "07desert" string from main()
-sub     eax, 0F9F4Ch ; EAX = Data section address
+mov     eax, [esi+0B3086h+1] ; Get the address of the MOV with the "DEBUGACTIVE" string
+sub     eax, 0FE1BCh ; EAX = Data section address
 mov     [edx+8], eax ; Data section address stored in the block in the 3rd 4 bytes
 mov     ebx, [esp]
 mov     [edx+12], ebx ; BIN file length stored in the block in the 4th 4 bytes
 
 add     esp, 8 ; Remove the saved things from the stack
 
-;mov     [esi+0EAFFBh], edx ; Allocated buffer address stored in the end of the code section
-; This above is commented out and the NOPs are here in case I see it's a good idea to free()
-; the memory at the end (and actually find a way of doing that). Though, I don't think it's
-; that bad. Will just leak this bit (more whatever the game leaks, anyway, but I can't fix that,
-; if it leaks anything, that is).
-; But the NOPs are really here just because then I don't need to go all over the offsets
-; on the instructions below if I want to change this. This way I just rewrite the NOP bytes.
-nop
-nop
-nop
-nop
+; Allocated buffer address stored in the end of the code section.
+; This is useful so that it can be freed on game exit. We'll already be modifying Code section's bytes (in the Patcher),
+; which is not supposed to be possible (as it's not writable by default). If we're already doing that, what's the
+; problem in changing one more thing...
+mov     [esi+0EAFFBh], edx
+
+;;;;;;;;;;;;;;;;;;;
+; Prepare the Patcher
+push    ebx
+
+mov     ecx, [edx+12] ; ECX = BIN file length
+add     ecx, edx ; ECX += code block address
+sub     ecx, 4 ; ECX = last address to search the Special Number on (the last 4 bytes)
+
+mov     ebx, edx ; EAX = block address (a copy to be used to find realMain())
+dec     ebx ; So that it's "-1". Then it starts at 0 and no jumps are needed.
+loop_find_main:
+	inc     ebx
+	cmp     ebx, ecx
+	jg      errorRealMainNotFound
+	cmp     dword ptr [ebx], 78563412h ; This constant is defined in the Patcher as SN_MAIN_FUNCTION (Special Number)
+	loopne  loop_find_main
+add     ebx, 4 ; EAX = main external function address (jump over the 32-bit Special Number)
 
 lea     eax, [esi+12345678h] ; START_PATCHER_STR
 push    eax
@@ -155,20 +209,12 @@ lea     ecx, [esi+0CA3B0h] ; printf_(format, ...) EAX
 call    ecx
 add     esp, 4
 
-;;;;;;;;;;;;;;;;;;;
-push    esi
-push    ebx
+;;;;;;;;;;
+; Call the Patcher
+push    edi
 
 ; Parameters for the Patcher
-mov     esi, edx ; ESI = block address (the Patcher requires ESI to be the block address)
-
-mov     ebx, edx ; EAX = block address (a copy to be used to find realMain())
-dec     ebx ; So that it's "-1". Then it starts at 0 and no jumps are needed.
-loop_find_main:
-	inc     ebx
-	cmp     dword ptr [ebx], 78563412h ; This constant is defined in the Patcher as SN_MAIN_FUNCTION (Special Number)
-	loopne  loop_find_main
-add     ebx, 4 ; EAX = main external function address (jump over the 32-bit Special Number)
+mov     edi, edx ; ESI = block address (the Patcher requires ESI to be the block address)
 
 ; In case anything goes wrong inside the Patcher (wrong out of what is expected - not an error and return false, I mean
 ; a serious unexpected error related with stack or Special Numbers or something like that) and it is able to return
@@ -178,9 +224,10 @@ xor     eax, eax
 
 call    ebx ; Call the Patcher's realMain() function
 
+pop     edi
+;;;;;;;;;;
+
 pop     ebx
-pop     esi
-;;;;;;;;;;;;;;;;;;;
 
 ; realMain's return type is 'bool'. That's 0 or 1, and when it's to put false, Watcom XORs AL with AL, not EAX, with EAX.
 ; Makes sense, because it's only 0 or 1, nothing else. So I've put the check with AL too (no need for EAX really if Watcom
@@ -188,6 +235,7 @@ pop     esi
 test    al, al
 jnz     printSuccess  ; Returned true, then all went fine
 jmp     printErrors   ; Returned false, then an error occurred
+;;;;;;;;;;;;;;;;;;;
 
 errorOpen:
 	lea     eax, [esi+12345678h] ; ERR_OPEN_STR
@@ -238,8 +286,8 @@ errorWrongVer:
 	movzx   cx, byte ptr [edx+3]
 	push    ecx ; BIN file type
 
-	mov     ecx, dword ptr [edx]
-	mov     dword ptr [esp], ecx ; The first 3 characters of the file (must say "LES")
+	mov     ecx, [edx]
+	mov     [esp], ecx ; The first 3 characters of the file (must say "LES")
 	mov     byte ptr [esp+3], 0  ; NULL terminate the string
 	push    eax ; Push the address of where the string is on the stack (stored in EAX)
 
@@ -248,6 +296,16 @@ errorWrongVer:
 	lea     ecx, [esi+0CA3B0h] ; printf_(format, ...) EAX
 	call    ecx
 	add     esp, 12
+	jmp     printFatalError
+
+errorRealMainNotFound:
+	pop     ebx
+	
+	lea     eax, [esi+12345678h] ; ERR_REALMAIN_NOT_FOUND
+	push    eax
+	lea     ecx, [esi+0CA3B0h] ; printf_(format, ...) EAX
+	call    ecx
+	add     esp, 4
 	jmp     printFatalError
 
 printFatalError:
@@ -289,13 +347,13 @@ add     esp, 4
 mov     ecx, esi ; ECX = Code section address
 
 ; Restore all registers
-push    ebp
-push    edi
-push    esi
-push    edx
-;push    ecx
-push    ebx
-push    eax
+pop     ebp
+pop     edi
+pop     esi
+pop     edx
+;pop     ecx
+pop     ebx
+pop     eax
 
 ; Execute the removed main() call, now with offsets and not effective addresses (no ESI to help here).
 ; EDIT: since ECX is set right after returning from this code cave and sub_13450() doesn't use it,
@@ -310,24 +368,28 @@ ret
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;; Strings
 
+; The strings are in the order they are requested in code for the manual patch to be easier.
+
 ; At most 80 chars per line, don't forget that.
 
 INIT_MSG db "----------------------------",0Dh,0Ah,"----- F1DP Loader v1.0 -----",0Dh,0Ah, 0
-END_MSG db  "----- F1DP Loader v1.0 -----",0Dh,0Ah,"----------------------------",0Dh,0Ah, 0
 
-BIN_FILE_NAME db "dospatch.bin", 0 ; At most 12 characters excluding the NULL one (DOS' 8.3 file names - 8 name chars, one dot, 3 chars the extension)
 PRT_BIN_FILE_NAME db "- BIN file containing the Patcher: dospatch.bin",0Dh,0Ah, 0
-START_PATCHER_STR db "- Patcher loaded correctly. Starting it now...",0Dh,0Ah, 0
+BIN_FILE_NAME db "dospatch.bin", 0 ; At most 12 characters excluding the NULL one (DOS' 8.3 file names - 8 name chars, one dot, 3 extension chars)
+START_PATCHER_STR db "- Patcher ready for action. Starting it...",0Dh,0Ah, 0
 
 ERR_OPEN_STR db "- [X] Error opening the BIN file.",0Dh,0Ah, 0
 ERR_FILE_LENGTH_STR db "- [X] Error getting the length of the BIN file.",0Dh,0Ah, 0
 ERR_MALLOC_STR db "- [X] Error allocating RAM memory space (%u bytes) for the Patcher.",0Dh,0Ah, 0
 ERR_READ_STR db "- [X] Error reading the BIN file.",0Dh,0Ah, 0
 ERR_WRNG_TYPE_STR db "- [X] Wrong BIN file type. Expecting type LES0, but got %s%d.",0Dh,0Ah, 0 ; [!!!] VERSION HERE
+ERR_REALMAIN_NOT_FOUND db "- [X] Main Patcher function not found! This should not happen...",0Dh,0Ah, 0
 ERR_LOAD_NO_PATCHES db 0Dh,0Ah,"- Press any key to proceed loading the game WITHOUT any patches...",0Dh,0Ah, 0
 PATCHER_SUCCESS db 0Dh,0Ah,"- The Patcher exited successfully! The game will now start automatically.",0Dh,0Ah, 0
 PATCHER_ERRORS db 0Dh,0Ah,"- The Patcher exited with errors! Please check the console.",0Dh,0Ah,
 				          "  Press any key to proceed loading the game...",0Dh,0Ah, 0
+
+END_MSG db  "----- F1DP Loader v1.0 -----",0Dh,0Ah,"----------------------------",0Dh,0Ah, 0
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
