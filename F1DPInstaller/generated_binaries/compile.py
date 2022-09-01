09844a9d991e861911f86a0754b1a054a5eb36ac
DADi590 <40151365+DADi590@users.noreply.github.com>
2022-09-01 01:06:29 +0100

    Create Open Watcom 2 toolchain for CLion
    No need for a Python script to compile Watcom stuff anymore. I'll still
    leave the script here though, for whoever wants to use it.

--- F1DPInstaller/generated_binaries/compile.py
+++ F1DPInstaller/generated_binaries/compile.py
@@ -27,11 +27,12 @@ try:
 	os.remove("F1DPINST.EXE")
 except:
 	pass
-compile_err = os.system("wcl386.exe " + all_C_files_ready + " -bt=DOS -l=dos4g -fpd -mf -oi -ol+ -oneatx -ot -wcd=119 -wcd=201 -we -wo -wx -ze") == 0
-try:
-	os.rename("main.exe", "F1DPINST.EXE")
-except:
-	pass
+compile_err = os.system("wcl386.exe " + all_C_files_ready + " -fe=F1DPINST -bt=DOS -l=dos4g -fpd -mf -oi -ol+ -oneatx -ot -wcd=119 -wcd=201 -we -wo -wx -ze") == 0
+
+exit(not compile_err)
+
+# Windows version discontinued. Why have one anyway? This will always run on DOS.
+
 print("\n----- NT -----")
 print("----- NT -----")
 print("----- NT -----\n")
@@ -39,11 +40,7 @@ try:
 	os.remove("F1DPInstallerWin.exe")
 except:
 	pass
-compile_err = compile_err and (os.system("wcl386.exe " + all_C_files_ready + " -fpd -mf -oi -ol+ -oneatx -ot -wcd=119 -wcd=201 -we -wo -wx -ze") == 0)
-try:
-	os.rename("main.exe", "F1DPInstallerWin.exe")
-except:
-	pass
+compile_err = compile_err and (os.system("wcl386.exe " + all_C_files_ready + " -fe=F1DPInstallerWin -fpd -mf -oi -ol+ -oneatx -ot -wcd=119 -wcd=201 -we -wo -wx -ze") == 0)
 # For testing (no optimizations, so errors come up decently, as already happened an important crash not happening
 # with optimizations, but happen without them, so disable them for debug and testing and only enable for releases (or
 # not even that, doesn't really matter in such a small program).
--- F1DPPatcher/generated_binaries/compile.py
+++ F1DPPatcher/generated_binaries/compile.py
@@ -20,7 +20,7 @@ print()
 # The exit() call below is so that this script exits with the same error code as Watcom.
 
 # For releases
-exit(os.system("wcl386.exe " + all_C_files_ready + " -bt=DOS -l=dos4g -fpd -mf -oi -ol+ -oneatx -ot -wcd=119 -we -wo -wx -ze"))
+exit(os.system("wcl386.exe " + all_C_files_ready + " -fe=F1DPPatcher -bt=DOS -l=dos4g -fpd -mf -oi -ol+ -oneatx -ot -wcd=119 -we -wo -wx -ze"))
 # For testing (no optimizations, so errors come up decently, as already happened an important crash not happening
 # with optimizations, but happen without them, so disable them for debug and testing and only enable for releases (or
 # not even that, doesn't really matter in such a small program).
